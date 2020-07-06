471e27970ad698921a76dcc697e2e29a7f76f2a4
Alex Kleider <alexkleider@protonmail.com>
2020-07-06 15:50:54 -0700

    First commit

---
+++ ft.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# File: ft.py (functional_tests.py)
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
+
+
+
